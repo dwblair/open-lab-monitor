34969ae45de46247ec139d2c612c9e6ed0080b2c
Don Blair <donblair@gmail.com>
2013-01-15 19:22:17 -0500

    changed pcb around

---
+++ pcb/OLM.cmd
@@ -0,0 +1,126 @@
+# Pin name action command file
+
+# Start of element L2
+ChangePinName(L2, 2, 2)
+ChangePinName(L2, 1, 1)
+
+# Start of element RN3A
+ChangePinName(RN3A, 1, 1)
+ChangePinName(RN3A, 2, 2)
+
+# Start of element RN3D
+ChangePinName(RN3D, 1, 1)
+ChangePinName(RN3D, 2, 2)
+
+# Start of element C8
+ChangePinName(C8, 2, -)
+ChangePinName(C8, 1, +)
+
+# Start of element C9
+ChangePinName(C9, 2, 2)
+ChangePinName(C9, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 2, 2)
+ChangePinName(L1, 1, 1)
+
+# Start of element Y1
+ChangePinName(Y1, 2, 2)
+ChangePinName(Y1, 1, 1)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element RN1D
+ChangePinName(RN1D, 1, 1)
+ChangePinName(RN1D, 2, 2)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element U?
+ChangePinName(U?, 3, +)
+ChangePinName(U?, 2, -)
+ChangePinName(U?, 1, Vout)
+ChangePinName(U?, 12, PB7/PCINT7/OC0A/OC1C/RTS)
+ChangePinName(U?, 30, PB6/PCINT6/OC1B/OC4B/ADC13)
+ChangePinName(U?, 24, AVCC)
+ChangePinName(U?, 43, GND)
+ChangePinName(U?, 17, XTAL1)
+ChangePinName(U?, 16, XTAL2)
+ChangePinName(U?, 13, RESET)
+ChangePinName(U?, 32, PC7/ICP3/CLK0/OC4A)
+ChangePinName(U?, 33, PE2/HWB)
+ChangePinName(U?, 7, VBus)
+ChangePinName(U?, 6, UCap)
+ChangePinName(U?, 5, UGnd)
+ChangePinName(U?, 4, D+)
+ChangePinName(U?, 3, +)
+ChangePinName(U?, 2, -)
+ChangePinName(U?, 1, Vout)
+ChangePinName(U?, 34, VCC)
+ChangePinName(U?, 35, GND)
+ChangePinName(U?, 39, PF4/ADC4/TCK)
+ChangePinName(U?, 40, PF1/ADC1)
+ChangePinName(U?, 41, PF0/ADC0)
+ChangePinName(U?, 36, PF7/ADC7/TDI)
+ChangePinName(U?, 37, PF6/ADC6/TDO)
+ChangePinName(U?, 38, PF5/ADC5/TMS)
+ChangePinName(U?, 23, GND)
+ChangePinName(U?, 15, GND)
+ChangePinName(U?, 42, AREF)
+ChangePinName(U?, 14, VCC)
+ChangePinName(U?, 44, AVCC)
+ChangePinName(U?, 31, PC6/OC3A/OC4A)
+ChangePinName(U?, 25, PD4/ICP1/ADC8)
+ChangePinName(U?, 21, PD3/TXD1/INT3)
+ChangePinName(U?, 20, PD2/RXD1/INT2)
+ChangePinName(U?, 19, PD1/SDA/INT1)
+ChangePinName(U?, 18, PD0/OC0B/SCL/INT0)
+ChangePinName(U?, 27, PD7/T0/OC4D/ADC10)
+ChangePinName(U?, 26, PD6/T1/OC4D/ADC9)
+ChangePinName(U?, 22, PD5/XCK1/CTS)
+ChangePinName(U?, 11, PB3/PDO/PCINT3/MISO)
+ChangePinName(U?, 10, PB2/PDI/PCINT2/MOSI)
+ChangePinName(U?, 9, PB1/PCINT1/SCLK)
+ChangePinName(U?, 8, PB0/SS/PCINT0)
+ChangePinName(U?, 29, PB5/PCINT5/OC1A/OC4B/ADC12)
+ChangePinName(U?, 28, PB4/PCINT4/ADC11)
